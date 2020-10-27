60a630af4873d74e49fcd72424dab8018ff8c605
nguyen vinh pha <pha.nv@neo-lab.vn>
2020-10-27 09:52:57 +0700

    keep caret position when executecommand

--- front-end/src/components/TextInputChat.vue
+++ front-end/src/components/TextInputChat.vue
@@ -8,149 +8,213 @@
 			<a :class="['icon', { 'selected': toolBarActive.formatBlock.active}]" href="javascript:void(0)" @click="format('formatBlock', 'blockquote'); toolBarActive.formatBlock.active = !toolBarActive.formatBlock.active"><i class="fa fa-quote-left"></i></a>
 			<a :class="['icon', 'icon-smile']" href="javascript:void(0)" @click="showEmojiBox"><i class="fa fa-smile-o"></i></a>
 		</div>
-		<div @click="checkActiveToolbar(); updateCurentSelection()" @keyup="checkActiveToolbar(); updateCurentSelection()" @input="emitHtml" id="htmlContent" contenteditable="true" class="text-input-editor">
+		<div @click="checkActiveToolbar(); updateCurentSelection()" @keyup="checkActiveToolbar(); updateCurentSelection()" @input="emitHtml" :id="idUniqueDivEdit" contenteditable="true" class="text-input-editor">
 		</div>
 		<EmojiBox @selectEmoji="selectEmoji" ref="emoji-box"/>
 	</div>
 </template>
 <script>
-import $ from 'jquery'
-import _ from 'lodash'
-import EmojiBox from './EmojiBox/EmojiBox'
+import $ from "jquery";
+import _ from "lodash";
+import EmojiBox from "./EmojiBox/EmojiBox";
 export default {
-	props: ['value'],
-	components: {
-		EmojiBox
-	},
-	data() {
-		return {
-			htmlContent: '',
-			cursorPos: 0,
-			classUnique: (new Date()).getTime().toString(),
-			toolBarActive: {
-				italic: {
-					selector: "i",
-					active: false
-				},
-				bold: {
-					selector: "b",
-					active: false
-				},
-				link: {
-					selector: "a",
-					active: false
-				},
-				insertunorderedlist: {
-					selector: "ul",
-					active: false
-				},
-				formatBlock: {
-					selector: "blockquote",
-					active: false
-				}
-			},
-			currentRange: null,
-			currentOffset: null
-		}
-	},
-	created() {
-		this.updateHtml(this.value)
-		this.emitHtml()
-	},
-	methods: {
-		format(type, value = null) {
-			document.execCommand(type, false, value)
-			$("#htmlContent *").removeAttr('style');
-			if (type === "formatBlock" && this.toolBarActive.formatBlock.active) {
-				document.execCommand('formatBlock', false, '<div>')
-			}
-			$('#htmlContent').focus()
-		},
-		updateHtml() {
-			this.htmlContent = $("#htmlContent").html()
-		},
-		checkActiveToolbar: _.debounce(function() {
-			for (let i in this.toolBarActive) {
-				this.toolBarActive[i]['active'] = this.caretIsChildIn(this.toolBarActive[i]['selector'])
-			}
-		}, 100),
-		caretIsChildIn(selector) {
-			let sel = null,
-				range = null
-			if (window.getSelection && (sel = window.getSelection()).rangeCount) {
-				range = sel.getRangeAt(0);
-				if ($(range.startContainer).closest(selector).length > 0) {
-					return true
-				}
-			}
-			return false
-		},
-		emitHtml() {
-			this.htmlContent = $('#htmlContent').html()
-			this.$emit('input', this.htmlContent)
-			const height = $(`.${this.classUnique} .text-input-toolbar`).outerHeight() + $(`.${this.classUnique} .text-input-editor`).outerHeight()
-			$(`.${this.classUnique}`).height(height+'px')
-			this.$emit('updateHeight', height)
-		},
-		updateCurentSelection() {
-			let sel
-			if (window.getSelection) {
-				sel = window.getSelection()
-				this.currentRange = sel.getRangeAt(0)
-				this.currentOffset = sel.focusOffset
-			}
-		},
-		getHtml() {
-			return this.htmlContent
-		},
-		setHtml(html) {
-			this.htmlContent = html
-			$('#htmlContent').html(html)
-			this.emitHtml()
-		},
-		selectEmoji(emoji) {
-			this.pasteHtmlAtCaret(emoji.char)
-		},
-		pasteHtmlAtCaret(html, spaceReplace = 0) {
-			var sel, range;
-			if (!this.currentRange) {
-				$('#htmlContent').focus()
-				this.updateCurentSelection()
-			}
-			if (window.getSelection) {
-				// IE9 and non-IE
-				sel = window.getSelection()
-				if (sel.getRangeAt && sel.rangeCount) {
-					range = this.currentRange
-					range.setStart(range.startContainer, this.currentOffset - spaceReplace)
-					range.setEnd(range.startContainer, this.currentOffset)
-					console.log(range)
-					range.deleteContents();
+    props: ["value"],
+    components: {
+        EmojiBox,
+    },
+    data() {
+        return {
+            htmlContent: "",
+            cursorPos: 0,
+            classUnique: new Date().getTime().toString(),
+            idUniqueDivEdit: new Date().getTime().toString(),
+            toolBarActive: {
+                italic: {
+                    selector: "i",
+                    active: false,
+                },
+                bold: {
+                    selector: "b",
+                    active: false,
+                },
+                link: {
+                    selector: "a",
+                    active: false,
+                },
+                insertunorderedlist: {
+                    selector: "ul",
+                    active: false,
+                },
+                formatBlock: {
+                    selector: "blockquote",
+                    active: false,
+                },
+            },
+            currentSelection: {
+                start: 0,
+                end: 0,
+            },
+        };
+    },
+    created() {
+        this.updateHtml(this.value);
+        this.emitHtml();
+    },
+    methods: {
+        format(type, value = null) {
+            this.replaceSelection();
+            document.execCommand(type, false, value);
+            $("#htmlContent *").removeAttr("style");
+            if (
+                type === "formatBlock" &&
+                this.toolBarActive.formatBlock.active
+            ) {
+                document.execCommand("formatBlock", false, "<div>");
+            }
+            this.updateCurentSelection();
+        },
+        updateHtml() {
+            this.htmlContent = $("#htmlContent").html();
+        },
+        checkActiveToolbar: _.debounce(function() {
+            for (let i in this.toolBarActive) {
+                this.toolBarActive[i]["active"] = this.caretIsChildIn(
+                    this.toolBarActive[i]["selector"]
+                );
+            }
+        }, 100),
+        caretIsChildIn(selector) {
+            let sel = null,
+                range = null;
+            if (
+                window.getSelection &&
+                (sel = window.getSelection()).rangeCount
+            ) {
+                range = sel.getRangeAt(0);
+                if ($(range.startContainer).closest(selector).length > 0) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        emitHtml() {
+            this.htmlContent = $(`#${this.idUniqueDivEdit}`).html();
+            this.$emit("input", this.htmlContent);
+            const height =
+                $(`.${this.classUnique} .text-input-toolbar`).outerHeight() +
+                $(`.${this.classUnique} .text-input-editor`).outerHeight();
+            $(`.${this.classUnique}`).height(height + "px");
+            this.$emit("updateHeight", height);
+        },
+        updateCurentSelection() {
+            let containerEl = document.getElementById(
+                `${this.idUniqueDivEdit}`
+            );
+            if (window.getSelection && document.createRange) {
+                let range = window.getSelection().getRangeAt(0);
+                let preSelectionRange = range.cloneRange();
+                preSelectionRange.selectNodeContents(containerEl);
+                preSelectionRange.setEnd(
+                    range.startContainer,
+                    range.startOffset
+                );
+                let start = preSelectionRange.toString().length;
 
-					var el = document.createElement("span");
-					el.innerHTML = html;
-					var frag = document.createDocumentFragment(), node, lastNode;
-					while ( (node = el.firstChild) ) {
-						lastNode = frag.appendChild(node);
-					}
-					range.insertNode(frag);
-					
-					if (lastNode) {
-						range = range.cloneRange();
-						range.setStartAfter(lastNode);
-					}
-				}
-			} else if ( (sel = document.selection) && sel.type != "Control") {
-				// IE < 9
-				var originalRange = sel.createRange();
-				originalRange.collapse(true);
-				sel.createRange().pasteHTML(html);
-			}
-			this.emitHtml()
-		},
-		showEmojiBox(event) {
-			this.$refs['emoji-box'].show(event.clientX, event.clientY)
-		}
-	}
-}
+                this.currentSelection = {
+                    start: start,
+                    end: start + range.toString().length,
+                };
+            } else if (document.selection && document.body.createTextRange) {
+                let selectedTextRange = document.selection.createRange();
+                let preSelectionTextRange = document.body.createTextRange();
+                preSelectionTextRange.moveToElementText(containerEl);
+                preSelectionTextRange.setEndPoint(
+                    "EndToStart",
+                    selectedTextRange
+                );
+                let start = preSelectionTextRange.text.length;
+
+                this.currentSelection = {
+                    start: start,
+                    end: start + selectedTextRange.text.length,
+                };
+            }
+        },
+        replaceSelection() {
+            let savedSel = this.currentSelection;
+            let containerEl = document.getElementById(
+                `${this.idUniqueDivEdit}`
+            );
+            if (window.getSelection && document.createRange) {
+                let charIndex = 0,
+                    range = document.createRange();
+                range.setStart(containerEl, 0);
+                range.collapse(true);
+                let nodeStack = [containerEl],
+                    node,
+                    foundStart = false,
+                    stop = false;
+
+                while (!stop && (node = nodeStack.pop())) {
+                    if (node.nodeType == 3) {
+                        let nextCharIndex = charIndex + node.length;
+                        if (
+                            !foundStart &&
+                            savedSel.start >= charIndex &&
+                            savedSel.start <= nextCharIndex
+                        ) {
+                            range.setStart(node, savedSel.start - charIndex);
+                            foundStart = true;
+                        }
+                        if (
+                            foundStart &&
+                            savedSel.end >= charIndex &&
+                            savedSel.end <= nextCharIndex
+                        ) {
+                            range.setEnd(node, savedSel.end - charIndex);
+                            stop = true;
+                        }
+                        charIndex = nextCharIndex;
+                    } else {
+                        let i = node.childNodes.length;
+                        while (i--) {
+                            nodeStack.push(node.childNodes[i]);
+                        }
+                    }
+                }
+
+                let sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            } else if (document.selection && document.body.createTextRange) {
+                let textRange = document.body.createTextRange();
+                textRange.moveToElementText(containerEl);
+                textRange.collapse(true);
+                textRange.moveEnd("character", savedSel.end);
+                textRange.moveStart("character", savedSel.start);
+                textRange.select();
+            }
+        },
+        getHtml() {
+            return this.htmlContent;
+        },
+        setHtml(html) {
+            this.htmlContent = html;
+            $(`#${this.idUniqueDivEdit}`).html(html);
+            this.emitHtml();
+        },
+        selectEmoji(emoji) {
+            this.pasteHtmlAtCaret(emoji.char);
+        },
+        pasteHtmlAtCaret(html) {
+            this.replaceSelection();
+            document.execCommand("insertHTML", true, html);
+            this.emitHtml();
+            this.updateCurentSelection();
+        },
+        showEmojiBox(event) {
+            this.$refs["emoji-box"].show(event.clientX, event.clientY);
+        },
+    },
+};
 </script>
